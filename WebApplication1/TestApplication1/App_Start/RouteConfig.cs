03a0a01ee46e4d73bcfe83113bf055b9629a5885
Tao Yuki <yukit123@outlook.com>
2018-10-12 18:04:19 +0800

    ad ,csv

--- WebApplication1/TestApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/TestApplication1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TestApplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional },
                 namespaces: new string[] { "TestApplication1.Controllers" }
             );
         }
